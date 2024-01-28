e36585878df4b98f462bc98209fac97a50161165
ajkessel <adam@rosi-kessel.org>
2024-01-28 15:49:08 -0500

    added customizable templates added "insecure" option to configure cross-posting from encrypted to unencrypted rooms

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 This is a [Maubot](https://github.com/maubot/maubot) plugin for use in a [Matrix](https://matrix.org/) chat room. The plugin allows you to define arbitrary emoji reactions that will cause messages tagged with the specified emoji to be automatically cross-posted to a different room.
 
-**Important security warning**: the current implementation will post from any room defined in the configuration to any other room defined in the configuration, regardless of encryption of the source and destination rooms. So, depending on how you configure it, you could have it post from an encrypted room to a non-encrypted room (or vice-versa), so long as your maubot client is in both rooms. I'll work on making this configurable in the future.
+Example use cases could be tagging messages with :bulb: to post them to an #ideas channel for furthere follow-up. Or if a message is off-topic, an emoji-reaction could cause the message to be reposted to the room with the correct topic for further discussion.
+
+**Important security warning**: if `insecure` is set to `true` (the default setting), the plugin will post from any room defined in the configuration to any other room defined in the configuration, regardless of encryption of the source and destination rooms. So, depending on how you configure it, you could have it post from an encrypted room to a non-encrypted room (or vice-versa), so long as your maubot client is in both rooms. If you want to prohibit this behavior, set `insecure` in config.yaml to `false`.
 
 You'll need to create a config.yaml (based on [example-config.yaml](example-config.yaml)) to specify mappings. For example:
 ```
 mapping:
+   �~_~R�: 'id
   🚋: 'public_transportation'
 ```
 Will cause any message tagged with the train emoji 🚋 to be posted to the local room with the alias #public_transportation. You can define as many actions as you want, although currently the plugin is limited to posting to one room per emoji reaction.
@@ -15,8 +18,6 @@ Note the YAML parser is fussy and needs a space after the colon for the mapping 
 
 ## TODO items
 
-* add configuration to disallow cross-posting from encrypted to unencrypted rooms
 * add in-room commands to define mappings
-* allow custom template for cross-posting
 * cull back unnecessary libraries
 * allow multiple room posting with single emoji
--- reacjibot/bot.py
+++ reacjibot/bot.py
@@ -4,7 +4,8 @@ import re
 from mautrix.client import Client
 from mautrix.types import (Event, MessageType, EventID, UserID, FileInfo, EventType, RoomID,
                             MediaMessageEventContent, TextMessageEventContent, ContentURI,
-                            MatrixURI, ReactionEvent, RedactionEvent, ImageInfo, RelationType)
+                            MatrixURI, ReactionEvent, RedactionEvent, ImageInfo, RelationType,
+                            EncryptedEvent)
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command, event
@@ -17,11 +18,20 @@ class Config(BaseProxyConfig):
         helper.copy("mapping")
         helper.copy("domain")
         helper.copy("repost")
+        helper.copy("insecure")
+        helper.copy("template")
 
 class ReacjiBot(Plugin):
-    reacji: dict
-    crossposted: dict
-    debug: bool
+    reacji: dict        # reacji->room mappings
+    crossposted: dict   # cache of cross-posted messages 
+    encrypted: dict     # cache of room encryption settings
+    allowed: dict       # cache of users allowed to cross-post
+    insecure: bool      # are users allowed to post from encrypted to unencrypted rooms
+    restrict: bool      # are only identified users allow to cross-post
+    debug: bool         # preference for debug logging
+    template: str       # template for cross-posting messages
+
+# UpdateReacji: re-process emoji->room mappings and look up rooms as required
     async def UpdateReacji(self) -> None:
         for key in self.config["mapping"]:
             room=self.config["mapping"][key]
@@ -33,39 +43,76 @@ class ReacjiBot(Plugin):
                try:
                  room = (await self.client.resolve_room_alias('#' + room)).room_id
                except:
-                 self.debug and self.log.debug(f"no room mapping found for {room}")
+                 self.debug and self.log.debug(f"no valid room mapping found for {room}")
                  room = ""
             self.debug and self.log.debug(f"mapping {key} to {room}")
             self.reacji[key] = room
 
+# IsEncrypted: check if room_id is an encrypted room, used if insecure is set to false
+    async def IsEncrypted(self, room_id) -> None:
+        self.debug and self.log.debug(f"checking for encryption on {room_id}")
+        if room_id in self.encrypted:
+           return self.encrypted[room_id]
+        else:
+           try:
+               if await self.client.get_state_event(room_id, EventType.ROOM_ENCRYPTION):
+                  is_encrypted = True
+           except:
+               is_encrypted = False
+           self.encrypted[room_id] = is_encrypted
+           return is_encrypted
+
+# start: load initial configuration and do sanity check
     async def start(self) -> None:
         self.config.load_and_update()
         self.reacji = {}
         self.crossposted = {}
-        self.debug = self.config["debug"]
-        self.debug and self.log.debug(f"verbose debugging enabled in config.yaml")
+        self.encrypted = {}
+        self.allowed = {}
+        self.template = '[%on](%ol): %m \n\n (%b)'
+        try:
+            self.debug = self.config["debug"]
+            self.debug and self.log.debug(f"verbose debugging enabled in config.yaml")
+            if self.config['template']:
+               self.template = self.config["template"]
+               self.debug and self.log.debug(f"got template {self.template}")
+            else: 
+               self.debug and self.log.debug(f"using default template")
+            self.insecure = self.config["insecure"]
+            self.restrict = self.config["restrict_users"]
+            if self.restrict:
+                self.allowed = self.config["allowed_users"]
+        except:
+            self.log.debug(f"error in configuration file: {error}")
         await self.UpdateReacji()
 
     async def on_external_config_update(self) -> None:
-        self.config.load_and_update()
-        await self.UpdateReacji()
+        await self.start()
 
+# generic_react: called when a reaction to a message event occurs; main guts of the plugin
+# TODO - find a better regexp that only matches emojis
     @command.passive(regex=re.compile(r"[^A-Za-z0-9]"), field=lambda evt: evt.content.relates_to.key, event_type=EventType.REACTION, msgtypes=None)
     async def generic_react(self, evt: ReactionEvent, key: Tuple[str]) -> None:
-        if self.config['restrict_users'] and len(self.config['allowed_users']) > 0 and evt.sender not in self.config['allowed_users']:
+        if self.restrict and evt.sender not in self.allowed:
            self.debug and self.log.debug(f"user {evt.sender} not allowed to cross-post")
            return
         source_evt = await self.client.get_event(evt.room_id, evt.content.relates_to.event_id)
         symbol = evt.content.relates_to.key
+        if not self.insecure:
+           is_source_encrypted = await self.IsEncrypted(evt.room_id)
         for key in self.reacji:
            if re.match(key,symbol):
                # assemble cross-post string if matching reacji is found
               target_id = self.reacji[key]
+               # check if this is an insecure cross-post and if so whether that is allowed
+              if not self.insecure and is_source_encrypted and not await self.IsEncrypted(target_id):
+                  self.debug and self.log.debug(f"insecure cross-posting not allowed, skipping")
+                  continue
                # first check to make sure this is not a re-post; if it has already been posted and repost is not true, skip
               if not self.config["repost"] and evt.content.relates_to.event_id in self.crossposted:
-                 if target_id in self.crossposted[evt.content.relates_to.event_id]:
-                    self.debug and self.log.debug(f"event {source_evt.content.body} already cross-posted, skipping")
-                    continue
+                  if target_id in self.crossposted[evt.content.relates_to.event_id]:
+                      self.debug and self.log.debug(f"event {source_evt.content.body} already cross-posted, skipping")
+                      continue
                # displayname: the display name for the original poster
               displayname = await self.client.get_displayname(source_evt.sender)
                # userlink: a hyperlink to the original poster's user ID
@@ -83,7 +130,12 @@ class ReacjiBot(Plugin):
                # xlinkback: the full hyperlinked string to the original message
               xlinkback = f"{xlink} by {xuserlink}"
                # message: the full message to be posted in the new room
-              message = f"[{displayname}]({userlink}): {body} {chr(10)}{chr(10)} ({xlinkback})"
+              message = self.template
+              message = message.replace('%on',displayname)
+              message = message.replace('%ol',str(userlink))
+              message = message.replace('%m',body)
+              message = message.replace('\\n',chr(10))
+              message = message.replace('%b',str(xlinkback))
               self.debug and self.log.debug(f"posting {message} to {target_id}")
               await self.client.send_markdown(target_id,message)
                # add post to the crossposted dictionary to avoid future reposts
